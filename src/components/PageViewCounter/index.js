8d7046d19dc911be9ce265a2666cc8b85e24351f
midasminnegal <m.m.minnegal@student.utwente.nl>
2024-10-22 17:49:15 +0200

    Make component customizable

--- src/components/PageViewCounter/index.js
+++ src/components/PageViewCounter/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
+import PropTypes from 'prop-types'
 import usePageViewCounter from '../../hooks/usePageViewCounter'
 
-function PageViewCounter() {
-  const [count, isLoading] = usePageViewCounter()
+function PageViewCounter({ doneLoadingCallback, loadingPlaceholder = '', settings = {} }) {
+  const [count, isLoading] = usePageViewCounter(settings)
 
-  return isLoading ? '' : count
+  useEffect(() => {
+    if (!isLoading && doneLoadingCallback) doneLoadingCallback()
+  }, [isLoading, doneLoadingCallback])
+
+  return isLoading ? loadingPlaceholder : count
+}
+
+PageViewCounter.propTypes = {
+  doneLoadingCallback: PropTypes.func,
+  loadingPlaceHolder: PropTypes.string,
+  settings: PropTypes.shape({
+    customKey: PropTypes.string,
+    onlyCountUniqueVisitors: PropTypes.bool,
+  }),
 }
 
 export default PageViewCounter
