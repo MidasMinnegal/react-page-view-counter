c145fa35f11c40fa0b3d5732eed80e73ea9626db
midasminnegal <m.m.minnegal@student.utwente.nl>
2024-10-22 15:44:55 +0200

    Implement counter api

--- src/hooks/usePageViewCounter.js
+++ src/hooks/usePageViewCounter.js
@@ -1,3 +1,19 @@
+import { useEffect, useState } from 'react'
+import { CounterAPI } from 'counterapi'
+
 export default function usePageViewCounter() {
-  return 0
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const counter = new CounterAPI()
+
+    const fetchData = async () => {
+      const { Count: newCount } = await counter.up('test', 'test')
+      setCount(newCount)
+    }
+
+    fetchData()
+  }, [])
+
+  return count
 }
